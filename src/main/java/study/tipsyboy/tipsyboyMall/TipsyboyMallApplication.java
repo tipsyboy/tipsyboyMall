4db4ce787cbdfac603b382ced6cb6222009c4b97
TipsyBoy <gudah13@naver.com>
2023-12-07 01:32:22 +0900

    Initial commit

---
+++ src/main/java/study/tipsyboy/tipsyboyMall/TipsyboyMallApplication.java
@@ -0,0 +1,13 @@
+package study.tipsyboy.tipsyboyMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipsyboyMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipsyboyMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/tipsyboy/tipsyboyMall/TipsyboyMallApplicationTests.java
@@ -0,0 +1,13 @@
+package study.tipsyboy.tipsyboyMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipsyboyMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
